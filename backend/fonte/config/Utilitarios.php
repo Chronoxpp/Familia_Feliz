588b00bc5b12e0a82b3662fc405bc06f342f558b
Lucas Dourado Rodrigues <lucasdouradochrono@gmail.com>
2025-04-08 06:34:45 -0400

    feat(ambiente): Criei uma classe para conter funcoes auxiliares, atualmente possui uma funcao para ler um arquivo .env

---
+++ backend/fonte/config/Utilitarios.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace fonte\config;
+
+class Utilitarios
+{
+    static function carregarEnv($caminho)
+    {
+        if(!$caminho)
+        {
+            $caminho = __DIR__ . '\.env';
+        }
+
+        if (!file_exists($caminho))
+        {
+            throw new Exception(".env não encontrado em: $caminho");
+        }
+
+        $linhas = file($caminho, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+        foreach ($linhas as $linha) {
+            if (strpos(trim($linha), '#') === 0) continue;
+
+            list($chave, $valor) = explode('=', $linha, 2);
+            $chave = trim($chave);
+            $valor = trim($valor);
+
+            // Remove aspas se existirem
+            $valor = trim($valor, "\"'");
+
+            putenv("$chave=$valor");
+            $_ENV[$chave] = $valor;
+        }
+    }
+}
