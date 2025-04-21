7017f63e28ef10cd81b0da6af10f784396720205
Lucas Dourado Rodrigues <lucasdouradochrono@gmail.com>
2025-04-21 08:14:37 -0400

    style: adiciona espacos extras para melhor separacao visual dos metodos da classe e importa as classes utilizadas pelo codigo

--- backend/fonte/servicos/FamiliaServicos.php
+++ backend/fonte/servicos/FamiliaServicos.php
@@ -4,7 +4,14 @@ namespace Fonte\servicos;
 
 use Error;
 use Fonte\modelos\Familia;
+use Fonte\modelos\Usuario;
 use Fonte\repositorios\FamiliaRepositorio;
+use mysql_xdevapi\Exception;
+
+require_once __DIR__ . '/../../modelos/Familia.php';
+require_once __DIR__ . '/../../modelos/Usuario.php';
+require_once __DIR__ . '/../../repositorios/FamiliaRepositorio.php';
+
 
 class FamiliaServicos
 {
@@ -14,71 +21,75 @@ class FamiliaServicos
         $this->repositorio = new FamiliaRepositorio();
     }
 
-    public function registrarFamilia(string $descricao)
-    {
-        $familia = new Familia(null, $descricao, null);
 
+    public function registrarFamilia(Familia $familia): Familia
+    {
         $this->refinarFamilia($familia);
 
-        $familia = $this->repositorio->registrarFamilia($familia);
+        $this->repositorio->registrarFamilia($familia);
 
         return $familia;
     }
 
-    public function consultarFamilias(Usuario $usuario)
-    {
-        $familias = $this->repositorio->consultarFamilias($usuario);
 
-        return $familias;
+    public function consultarFamilias(int $idUsuario): array
+    {
+        return $this->repositorio->consultarFamilias($idUsuario);
     }
 
-    public function alterarFamilia(Familia $familia)
-    {
-        $familia = $this->refinarFamilia($familia);
 
-        $familia = $this->repositorio->alterarFamilia($familia);
+    public function alterarFamilia(Familia $familia): Familia
+    {
+        $this->refinarFamilia($familia);
+
+        $this->repositorio->alterarFamilia($familia);
 
         return $familia;
     }
 
-    public function deletarFamilia($familia)
+
+    public function deletarFamilia(Familia $familia): Familia
     {
-        return $this->repositorio->deletarFamilia($familia) || false;
+        return $this->repositorio->deletarFamilia($familia);
     }
 
-    public function adicionarMembro(Familia $familia, Usuario $membro)
-    {
-        $familia = $this->refinarFamilia($familia);
-        $membro = $this->refinarMembro($membro);
 
-        $familia = $this->repositorio->adicionarMembro($familia, $membro);
+    public function adicionarMembro(Familia $familia, Usuario $membro): Familia
+    {
+        $this->refinarFamilia($familia);
+        $this->refinarMembro($membro);
+
+        $this->repositorio->adicionarMembro($familia, $membro);
 
         return $familia;
     }
 
-    public function removerMembro(Familia $familia, Usuario $membro)
+
+    public function removerMembro(Familia $familia, Usuario $membro): Familia
     {
-        $familia = $this->repositorio->removerMembro($familia, $membro);
+        $this->repositorio->removerMembro($familia, $membro);
 
         return $familia;
     }
 
-    private function refinarFamilia(Familia $familia)
+
+    private function refinarFamilia(Familia $familia): Familia
     {
-        if ($familia->getDescricao() == false)
-        {
-            throw new Error('Deve ser informado um nome para a família!');
-        }
         $familia->setDescricao(trim($familia->getDescricao()));
+        if (! $familia->getDescricao())
+        {
+            throw new Exception('A familia deve possuir um nome/descrição!');
+        }
 
         return $familia;
     }
 
-    private function refinarMembro(Usuario $membro)
+
+    private function refinarMembro(Usuario $membro): Usuario
     {
-        if ($membro->getNome() == false)
+        if (! $membro->getNome())
         {
-            throw new Error('Membro da família sem nome, todo membro deve possuir um nome!');
+            throw new Exception('Membro da família sem nome, todo membro deve possuir um nome!');
         }
 
         return $membro;
